27929a6c07be37f5b59cf46cda1ab0cd4a188648
Eduardo Argollo <eargollo@gmail.com>
2023-09-10 10:48:54 -0700

    Signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example user",
+                                          email: "user@valid.com",
+                                          password: "foobar",
+                                          password_confirmation: "foobar"
+                                }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
 end
