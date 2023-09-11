3a0daed5946c95a0b632c890bc0fd4862e1d397e
Eduardo Argollo <eargollo@gmail.com>
2023-09-11 14:34:03 -0700

    invalid login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page
+      render 'new', notice: 'Log in successful'
+    else
+      # Create an error message
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   root "static_pages#home"
+
+  # Static pages
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
+  # Users
   resources :users
+  get '/signup', to: 'users#new'
+
+  # Sessions
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
