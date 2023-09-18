ea3cd8b5c15f1eb89e033148af6bfe64786ad6ee
Eduardo Argollo <eargollo@gmail.com>
2023-09-17 20:52:39 -0700

    Protect edit to user itself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -46,6 +47,15 @@ class UsersController < ApplicationController
     unless logged_in?
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      # flash[:danger] = "You are not authorized to edit this user."
+      redirect_to root_url, status: :see_other
     end
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
 module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
+        # Guard against session replay attacks.
+        # see https://bit.ly/33UvK0w for more.
+        session[:session_token] = user.session_token
     end
 
     def remember(user)
@@ -17,7 +20,10 @@ module SessionsHelper
 
     def current_user
         if (user_id = session[:user_id])
-            @current_user ||= User.find_by(id: user_id)
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end 
         elsif (user_id = cookies.encrypted[:user_id])
             user = User.find_by(id: user_id)
             if user&.authenticated?(cookies[:remember_token])
@@ -35,5 +41,9 @@ module SessionsHelper
         forget(current_user)
         reset_session
         @current_user = nil
+    end
+
+    def current_user?(user)
+        user && user == current_user
     end
 end
