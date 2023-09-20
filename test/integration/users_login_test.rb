81180d9d7bdc4daafa26016fedf2afb1a523030d
Eduardo Argollo <eargollo@gmail.com>
2023-09-20 13:44:42 -0700

    Refactor tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup 
     @user = users(:michael)
   end 
+end
 
+class InvalidPasswordTest < UsersLogin
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -20,30 +22,54 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information followed by logout" do
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'password' } }
+class ValidLogin < UsersLogin
+  def setup 
+    super
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test "valid log in" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
 
+class Logout < ValidLogin
+  def setup 
+    super
     delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
     # Simulate a user clicking logout in a second window.
-    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+end
 
+class RememberingTest < UsersLogin
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_not cookies[:remember_token].blank?
