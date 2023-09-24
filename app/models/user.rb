4502b11c36528551caebb8e7aa0518fde1b76778
Eduardo Argollo <eargollo@gmail.com>
2023-09-24 16:15:14 -0700

    Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, 
+    validates :email, presence: true, length: { maximum: 255 },
         format: { with: VALID_EMAIL_REGEX},
         uniqueness: true
 
@@ -15,7 +16,7 @@ class User < ApplicationRecord
 
     # Returns the hash digest of the given string.
     def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
             BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
